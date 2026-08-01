7a103400416c21aed020782ab4e6595eb2be0e4a
SaneKKelasev <55653855+SaneKKelasev@users.noreply.github.com>
2026-08-01 19:57:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
+    plugins: [ 
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,

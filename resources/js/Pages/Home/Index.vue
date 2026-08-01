075f72c2bdddf656cd3e05c075dc0a96d500d686
sanekkelasev <sanekkelasev>
2026-08-01 19:43:52 +0300

    install inertia, vue and setting vite

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const title = ref('PortfolioHub');
+
+    function changeTitle() {
+        title.value = 'Hello';
+    }
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+
+    <button type="button" @click="changeTitle">
+        Изменить заголовок
+    </button>
+</template>   
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-//
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+
+const pages = import.meta.glob('./Pages/**/*.vue');
+
+createInertiaApp({
+    resolve: (name) => {
+        const page = pages[`./Pages/${name}.vue`];
+
+        if (! page) {
+            throw new Error(`Page not found: ${name}`);
+        }
+
+        return page(); 
+    },
+
+    setup({ el, App, props, plugin }) {
+        return createApp({
+            render: () => h(App, props),
+        })
+            .use(plugin)
+            .mount(el);
+    },
+});

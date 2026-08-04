f2e56abc1ea9462a3befd5f03cfc5838855a329b
sanekkelasev <sanekkelasev>
2026-08-04 13:47:22 +0300

    Add project gallery and technologies

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -1,20 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
-class HomePageTest extends TestCase
+final class HomePageTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_home_page_displays_projects_page(): void
+    {
+        $this->get('/')
+            ->assertOk()
+            ->assertInertia(
+                fn (Assert $page): Assert => $page
+                    ->component('Home/Index')
+                    ->has('projects', 0),
+            );
     }
 }
